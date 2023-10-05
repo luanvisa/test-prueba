9baf50218315acd10d2ed7fa00bb7f49948a6f71
luanvisa <informatica@luanvi.es>
2023-10-05 13:56:45 +0200

    push all

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace gitHub
     static void Main(string[] args)
     {
       int sum1 = 100 + 50;     // 150 (100 + 50)
-      int sum2 = sum1 + 300;   // 400 (150 + 300)
+      int sum2 = sum1 + 300;   // 405 (150 + 300)
       int sum3 = sum2 + sum2;  // 800 (400 + 400)
       Console.WriteLine(sum1);
       Console.WriteLine(sum2);
